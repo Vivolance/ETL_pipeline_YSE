0c54513d19343aae723cf8be031ce743b7cbcedc
Elson Chan <elsonccjgmail.com>
2024-05-14 14:49:26 +0800

    Added conftest file for Integration testing

---
+++ integration_tests/conftest.py
@@ -0,0 +1,9 @@
+"""
+Contains fixture to all tests
+"""
+from typing import Any
+import toml
+
+
+def integration_test_db_config() -> dict[str, Any]:
+    return toml.load("integration_tests/config.toml")["database"]
