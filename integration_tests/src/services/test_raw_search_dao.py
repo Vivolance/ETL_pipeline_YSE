50ed7870057cd83fc10040216f69a7cc7f656ae2
Elson Chan <elsonccjgmail.com>
2024-05-17 20:59:57 +0800

    Added Integration test for test_fetch_searches_for_user

--- integration_tests/src/services/test_raw_search_dao.py
+++ integration_tests/src/services/test_raw_search_dao.py
@@ -4,7 +4,7 @@ import pytest
 
 from integration_tests.conftest import integration_test_db_config
 from integration_tests.src.utils.clear_tables import ClearTables
-from integration_tests.src.utils.engine import dummy_uuid
+from integration_tests.src.utils.engine import dummy_uuid, dummy_uuid_2
 from integration_tests.src.utils.fetch import Fetch
 from integration_tests.src.utils.insert import Insert
 from src.models.search_results import SearchResults
@@ -52,7 +52,7 @@ class TestRawSearchResult:
     @pytest.mark.asyncio_cooperative
     async def test_fetch_all_searches(self) -> None:
         await ClearTables.clear_users_table()
-        await ClearTables.clear_extracted_search_results_table()
+        await ClearTables.clear_search_results_table()
         users: list[User] = [
             User(
                 user_id=str(dummy_uuid),
@@ -77,5 +77,60 @@ class TestRawSearchResult:
 
         results_row: list[SearchResults] = await RAW_SEARCH_DAO.fetch_all_searches()
         assert results_row == search_results
-        await ClearTables.clear_extracted_search_results_table()
+        await ClearTables.clear_search_results_table()
+        await ClearTables.clear_users_table()
+
+    @pytest.mark.asyncio_cooperative
+    async def test_fetch_searches_for_user(self) -> None:
+        """
+        TODO: To be worked on. user_id is a primary key, how do we create multiple rows
+        with similar useR_id but different searches for different time period?
+        """
+        await ClearTables.clear_users_table()
+        await ClearTables.clear_search_results_table()
+        users: list[User] = [
+            User(
+                user_id=str(dummy_uuid),
+                created_at=datetime(year=2024, month=5, day=15, hour=15),
+            ),
+            User(
+                user_id=str(dummy_uuid_2),
+                created_at=datetime(year=2024, month=5, day=15, hour=15),
+            ),
+        ]
+        for user in users:
+            await Insert.insert_user(user)
+
+        search_results: list[SearchResults] = [
+            SearchResults(
+                search_id="dummy id",
+                user_id=str(dummy_uuid),
+                search_term="dummy search term",
+                result="dummy results",
+                created_at=datetime(year=2024, month=5, day=15, hour=15),
+            ),
+            SearchResults(
+                search_id="dummy id 2",
+                user_id=str(dummy_uuid),
+                search_term="dummy search term 2",
+                result="dummy results 2",
+                created_at=datetime(year=2024, month=5, day=15, hour=16),
+            ),
+            SearchResults(
+                search_id="dummy id",
+                user_id=str(dummy_uuid_2),
+                search_term="dummy search term",
+                result="dummy results",
+                created_at=datetime(year=2024, month=5, day=15, hour=16),
+            ),
+        ]
+
+        for search_result in search_results:
+            await Insert.insert_search_search_results(search_result)
+
+        results_row: list[SearchResults] = await RAW_SEARCH_DAO.fetch_searches_for_user(
+            str(dummy_uuid_2), datetime(year=2024, month=5, day=15, hour=16)
+        )
+        assert results_row == search_results
+        await ClearTables.clear_search_results_table()
         await ClearTables.clear_users_table()
